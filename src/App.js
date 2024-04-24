4b8b83c7be245bf4d635c2297a7459696a3b72d9
himanshu shukla <hemi8085@gmail.com>
2024-04-24 23:18:41 +0530

    implemented the home page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-// import React from "react";
-// import "./App.css";
-// import Card from "./components/card/Card";
-// import Heading from "./components/navbar/Navbar";
-// import Footer from "./components/footer/Footer";
-// import { EcomProvider } from "./components/store/EcomContext";
-// import CartModal from "./components/modal/CartModal";
-
-// function App() {
-//   return (
-//     <EcomProvider>
-//       <Heading />
-//       <Card />
-//       <Footer />
-//       <CartModal />
-//     </EcomProvider>
-//   );
-// }
-
-// export default App;
-// App.js
-
-// App.js
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
@@ -31,6 +7,7 @@ import Footer from "./components/footer/Footer";
 import { EcomProvider } from "./components/store/EcomContext";
 import CartModal from "./components/modal/CartModal";
 import AboutUs from "./components/pages/AboutUs";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -38,7 +15,8 @@ function App() {
       <EcomProvider>
         <Heading />
         <Routes>
-          <Route path="/" element={<Card />} />
+          <Route path="/store" element={<Card />} />
+          <Route path="/" element = {<Home/>}/>
           <Route path="/about" element={<AboutUs />} />
         </Routes>
         <Footer />
